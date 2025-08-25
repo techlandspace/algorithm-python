c063b282d3a57af50aa8457c7a7beb0c6175e470
liquanfeng <quanfeng1024@gmail.com>
2025-08-25 18:10:27 +0800

    update: 更新项目目录

--- src/algorithm/core/data_structures/hello_world.py
+++ src/algorithm/core/data_structures/hello_world.py
@@ -1,6 +1,13 @@
+def test():
+    alist = list(range(10))
+    print(alist)
+
+
 if __name__ == '__main__':
     print('Hello World!')
     print('Hello'.__add__('World'))
     print('Hello' + 'World')
     for i in range(10) :
         print(i, end="*")
+    print()
+    test()
